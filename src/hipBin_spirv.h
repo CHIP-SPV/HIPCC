c233723f585701e0c234607a0d61ba6a7307c895
Paulius Velesko <pvelesko@gmail.com>
2025-10-01 17:58:58 -0500

    macos

--- src/hipBin_spirv.h
+++ src/hipBin_spirv.h
@@ -561,7 +561,7 @@ bool HipBinSpirv::detectPlatform() {
    */
 
   HipInfo hipInfo;
-  fs::path currentBinaryPath = fs::canonical("/proc/self/exe");
+  fs::path currentBinaryPath = getExecutablePath();
   currentBinaryPath = currentBinaryPath.parent_path();
   fs::path sharePathBuild = currentBinaryPath.string() + "/../share";
   fs::path sharePathInstall =
